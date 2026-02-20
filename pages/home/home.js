3e234eee8f9f7f12751971147c5764a20bf894fd
352astro <1798701009@qq.com>
2026-02-20 21:35:23 +0800

    尝试调试3d模型

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    width: 300,
+    height: 300,
+    renderWidth: 300,
+    renderHeight: 300,
   },
   /**
    * 
@@ -20,8 +23,16 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
+  onLoad() {
+    const info = wx.getSystemInfoSync();
+    const width = info.windowWidth;
+    const height = info.windowHeight;
+    const dpi = info.pixelRatio;
+    this.setData({
+      width, height,
+      renderWidth: width * dpi,
+      renderHeight: height * dpi
+    });
   },
 
   /**
